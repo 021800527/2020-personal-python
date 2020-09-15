e278f1e20ef1ce0d4d889bc0ee5212656cd1305f
021800527 <978621025@qq.com>
2020-09-16 00:31:05 +0800

    message

--- GHAnalysis.py
+++ GHAnalysis.py
@@ -3,113 +3,133 @@ import os
 import argparse
 
 
-def initialization(path):
-    json_list = []
-    dict_address = path
-    user_4event_num = {}
-    repo_4event_num = {}
-    user_repo_4event_num = {}
-    for root, dic, files in os.walk(dict_address):
-        for f in files:
-            if f[-5:] == '.json' and f[-6] != '0' and f[-6] != '1' and f[-6] != '2':
-                json_path = f
-                x = open(dict_address+'\\'+json_path, 'r', encoding='utf-8').read()
-                str_list = [_x for _x in x.split('\n') if len(_x) > 0]
-                for i, _str in enumerate(str_list):
-                    # 个人用户的四个事件
-                    json_list.append(json.loads(_str))
-                    str_key = json_list[i]['actor']['login']
-                    # 初始化 如果非空就继续往下
-                    if user_4event_num.get(str_key) is None:
-                        user_4event_num[str_key] = {}
-                    # 初始化 不存在就创建，存在在后面处理
-                    if user_4event_num[str_key].get('IssuesEvent') is None:
-                        user_4event_num[str_key].update({'IssuesEvent': 0})
-                    if user_4event_num[str_key].get('PushEvent') is None:
-                        user_4event_num[str_key].update({'PushEvent': 0})
-                    if user_4event_num[str_key].get('IssueCommentEvent') is None:
-                        user_4event_num[str_key].update({'IssueCommentEvent': 0})
-                    if user_4event_num[str_key].get('PullRequestEvent') is None:
-                        user_4event_num[str_key].update({'PullRequestEvent': 0})
-                    if json_list[i]['type'] in user_4event_num[str_key].keys():
-                        user_4event_num[str_key][json_list[i]['type']] = user_4event_num[str_key][json_list[i]['type']] + 1
-                    # 项目的四种事件
-                    str_key = json_list[i]['repo']['name']
-                    # 初始化 如果非空就继续往下
-                    if repo_4event_num.get(str_key) is None:
-                        repo_4event_num[str_key] = {}
-                    # 初始化 不存在就创建，存在在后面处理
-                    if repo_4event_num[str_key].get('IssuesEvent') is None:
-                        repo_4event_num[str_key].update({'IssuesEvent': 0})
-                    if repo_4event_num[str_key].get('PushEvent') is None:
-                        repo_4event_num[str_key].update({'PushEvent': 0})
-                    if repo_4event_num[str_key].get('IssueCommentEvent') is None:
-                        repo_4event_num[str_key].update({'IssueCommentEvent': 0})
-                    if repo_4event_num[str_key].get('PullRequestEvent') is None:
-                        repo_4event_num[str_key].update({'PullRequestEvent': 0})
-                    if json_list[i]['type'] in repo_4event_num[str_key].keys():
-                        repo_4event_num[str_key][json_list[i]['type']] = repo_4event_num[str_key][json_list[i]['type']] + 1
-                    # 每一个人在每一个项目的 4 种事件的数量
-                    new_str_key = json_list[i]['repo']['name']
-                    str_key = json_list[i]['actor']['login']
-                    # 初始化 如果非空就继续往下
-                    if user_repo_4event_num.get(str_key) is None:
-                        user_repo_4event_num[str_key] = {}
-                        # 套娃第二步，先把项目名放进去
-                    if user_repo_4event_num[str_key].get(new_str_key) is None:
-                        user_repo_4event_num[str_key][new_str_key] = {}
-                    if user_repo_4event_num[str_key][new_str_key].get('IssuesEvent') is None:
-                        user_repo_4event_num[str_key][new_str_key].update({'IssuesEvent': 0})
-                    if user_repo_4event_num[str_key][new_str_key].get('PushEvent') is None:
-                        user_repo_4event_num[str_key][new_str_key].update({'PushEvent': 0})
-                    if user_repo_4event_num[str_key][new_str_key].get('IssueCommentEvent') is None:
-                        user_repo_4event_num[str_key][new_str_key].update({'IssueCommentEvent': 0})
-                    if user_repo_4event_num[str_key][new_str_key].get('PullRequestEvent') is None:
-                        user_repo_4event_num[str_key][new_str_key].update({'PullRequestEvent': 0})
-                    if json_list[i]['type'] in user_repo_4event_num[str_key][new_str_key].keys():
-                        user_repo_4event_num[str_key][new_str_key][json_list[i]['type']] = user_repo_4event_num[str_key][new_str_key][json_list[i]['type']] + 1
-    with open('0.json', 'w', encoding='utf-8') as f:
-        json.dump(user_4event_num, f)
-    with open('1.json', 'w', encoding='utf-8') as f:
-        json.dump(repo_4event_num, f)
-    with open('2.json', 'w', encoding='utf-8') as f:
-        json.dump(user_repo_4event_num, f)
+class Data:
+    def __init__(self, dict_address: int = None, reload: int = 0):
+        if reload == 1:
+            self.__init(dict_address)
+        if dict_address is None and not os.path.exists('1.json') and not os.path.exists('2.json') and not os.path.exists('3.json'):
+            raise RuntimeError('error: init failed')
+        x = open('1.json', 'r', encoding='utf-8').read()
+        self.__4Events4PerP = json.loads(x)
+        x = open('2.json', 'r', encoding='utf-8').read()
+        self.__4Events4PerR = json.loads(x)
+        x = open('3.json', 'r', encoding='utf-8').read()
+        self.__4Events4PerPPerR = json.loads(x)
+
+    def __init(self, dict_address: str):
+        json_list = []
+        for root, dic, files in os.walk(dict_address):
+            for f in files:
+                if f[-5:] == '.json':
+                    json_path = f
+                    x = open(dict_address+'\\'+json_path,
+                             'r', encoding='utf-8').read()
+                    str_list = [_x for _x in x.split('\n') if len(_x) > 0]
+                    for i, _str in enumerate(str_list):
+                        try:
+                            json_list.append(json.loads(_str))
+                        except:
+                            pass
+        records = self.__listOfNestedDict2ListOfDict(json_list)
+        self.__4Events4PerP = {}
+        self.__4Events4PerR = {}
+        self.__4Events4PerPPerR = {}
+        for i in records:
+            if not self.__4Events4PerP.get(i['actor__login'], 0):
+                self.__4Events4PerP.update({i['actor__login']: {}})
+                self.__4Events4PerPPerR.update({i['actor__login']: {}})
+            self.__4Events4PerP[i['actor__login']][i['type']
+                                         ] = self.__4Events4PerP[i['actor__login']].get(i['type'], 0)+1
+            if not self.__4Events4PerR.get(i['repo__name'], 0):
+                self.__4Events4PerR.update({i['repo__name']: {}})
+            self.__4Events4PerR[i['repo__name']][i['type']
+                                       ] = self.__4Events4PerR[i['repo__name']].get(i['type'], 0)+1
+            if not self.__4Events4PerPPerR[i['actor__login']].get(i['repo__name'], 0):
+                self.__4Events4PerPPerR[i['actor__login']].update({i['repo__name']: {}})
+            self.__4Events4PerPPerR[i['actor__login']][i['repo__name']][i['type']
+                                                          ] = self.__4Events4PerPPerR[i['actor__login']][i['repo__name']].get(i['type'], 0)+1
+        with open('1.json', 'w', encoding='utf-8') as f:
+            json.dump(self.__4Events4PerP,f)
+        with open('2.json', 'w', encoding='utf-8') as f:
+            json.dump(self.__4Events4PerR,f)
+        with open('3.json', 'w', encoding='utf-8') as f:
+            json.dump(self.__4Events4PerPPerR,f)
+
+    def __parseDict(self, d: dict, prefix: str):
+        _d = {}
+        for k in d.keys():
+            if str(type(d[k]))[-6:-2] == 'dict':
+                _d.update(self.__parseDict(d[k], k))
+            else:
+                _k = f'{prefix}__{k}' if prefix != '' else k
+                _d[_k] = d[k]
+        return _d
+
+    def __listOfNestedDict2ListOfDict(self, a: list):
+        records = []
+        for d in a:
+            _d = self.__parseDict(d, '')
+            records.append(_d)
+        return records
+
+    def getEventsUsers(self, username: str, event: str) -> int:
+        if not self.__4Events4PerP.get(username,0):
+            return 0
+        else:
+            return self.__4Events4PerP[username].get(event,0)
+
+    def getEventsRepos(self, reponame: str, event: str) -> int:
+        if not self.__4Events4PerR.get(reponame,0):
+            return 0
+        else:
+            return self.__4Events4PerR[reponame].get(event,0)
+
+    def getEventsUsersAndRepos(self, username: str, reponame: str, event: str) -> int:
+        if not self.__4Events4PerP.get(username,0):
+            return 0
+        elif not self.__4Events4PerPPerR[username].get(reponame,0):
+            return 0
+        else:
+            return self.__4Events4PerPPerR[username][reponame].get(event,0)
 
 
-def inquire_user_event(user, event):
-    with open('0.json', 'r',encoding='utf-8') as f:
-        dict1 = json.load(f)
-        print(dict1.get(user).get(event))
+class Run:
+    def __init__(self):
+        self.parser = argparse.ArgumentParser()
+        self.data = None
+        self.argInit()
+        print(self.analyse())
 
+    def argInit(self):
+        self.parser.add_argument('-i', '--init')
+        self.parser.add_argument('-u', '--user')
+        self.parser.add_argument('-r', '--repo')
+        self.parser.add_argument('-e', '--event')
 
-def inquire_repo_event(repo, event):
-    with open('1.json', 'r',encoding='utf-8') as f:
-        dict1 = json.load(f)
-        print(dict1.get(repo).get(event))
-
-
-def inquire_user_repo_event(user, repo, event):
-    with open('2.json', 'r',encoding='utf-8') as f:
-        dict1 = json.load(f)
-        print(dict1.get(user).get(repo).get(event))
+    def analyse(self):
+        if self.parser.parse_args().init:
+            self.data = Data(self.parser.parse_args().init, 1)
+            return 0
+        else:
+            if self.data is None:
+                self.data = Data()
+            if self.parser.parse_args().event:
+                if self.parser.parse_args().user:
+                    if self.parser.parse_args().repo:
+                        res = self.data.getEventsUsersAndRepos(
+                            self.parser.parse_args().user, self.parser.parse_args().repo, self.parser.parse_args().event)
+                    else:
+                        res = self.data.getEventsUsers(
+                            self.parser.parse_args().user, self.parser.parse_args().event)
+                elif self.parser.parse_args().repo:
+                    res = self.data.getEventsRepos(
+                        self.parser.parse_args().repo, self.parser.parse_args().event)
+                else:
+                    raise RuntimeError('error: argument -l or -c are required')
+            else:
+                raise RuntimeError('error: argument -e is required')
+        return res
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-u", "--user")
-    parser.add_argument("-e", "--event")
-    parser.add_argument("-i", "--address")
-    parser.add_argument("-r", "--repo")
-    args = parser.parse_args()
-    if args.address is not None:
-        initialization(args.address)
-        if args.address is None and not os.path.exists('1.json') and not os.path.exists('2.json') and not os.path.exists('3.json'):
-            print("初始化失败,本地文件不可写入")
-        else:
-            print("初始化成功")
-    if args.user is not None and args.event is not None and args.repo is None:
-        inquire_user_event(args.user,args.event)
-    if args.repo is not None and args.event is not None and args.user is None:
-        inquire_repo_event(args.repo, args.event)
-    if args.user is not None and args.repo is not None and args.event is not None:
-        inquire_user_repo_event(args.user, args.repo, args.event)
+    a = Run()
